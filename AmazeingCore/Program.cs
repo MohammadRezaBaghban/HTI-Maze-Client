77892d9d93c3e8399b736d26fd1e815a08d10e62
Mrbh <Mrbhmr@gmail.com>
2021-03-22 12:08:20 +0100

    Refactor: ConsoleLogging and Program Class

--- AmazeingCore/Program.cs
+++ AmazeingCore/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -8,30 +7,27 @@ namespace AmazeingCore
 {
     class Program
     {
-        private static AmazeingClient client;
-        private static Random rand = new Random();
-        private static MazeInfo chosenMaze = null;
+        private static AmazeingClient _client;
+        private static async Task<PlayerInfo> ClientInfo() => await _client.GetPlayerInfo();
 
-        static async Task Main(string[] args)
+        private static async Task Main()
         {
             await Connection_Initialization();
-            await ConsoleLogging.Client_Info(client);
-
             await Traverse_Mazes();
-
-            Console.WriteLine("You have finished all the Mazes:\n");
-            await ConsoleLogging.Client_Info(client);
             Console.ReadLine();
         }
 
         public static async Task Traverse_Mazes()
         {
-            var mazesList = (await client.AllMazes()).OrderBy(x => x.TotalTiles).ToList();
+            var mazesList = (await _client.AllMazes()).OrderBy(x => x.TotalTiles).ToList();
             foreach (var maze in mazesList)
             {
                 ConsoleLogging.Mazes_Info(mazesList);
                 await Traverse.Start(maze);
             }
+
+            Console.WriteLine("You have finished all the Mazes:\n");
+            ConsoleLogging.Client_Info(await ClientInfo());
         }
 
         public static async Task Connection_Initialization()
@@ -40,14 +36,15 @@ namespace AmazeingCore
             var authorization_Key = "HTI Thanks You [e48a]";
 
             httpClient.DefaultRequestHeaders.Add("Authorization", authorization_Key);
-            client = new AmazeingClient("https://maze.hightechict.nl/", httpClient);
-            Traverse.Client = client;
+            _client = new AmazeingClient("https://maze.hightechict.nl/", httpClient);
+            Traverse.Client = _client;
 
-            await client.ForgetPlayer();
+            await _client.ForgetPlayer();
+            await _client.RegisterPlayer(name: "MohammadReza");
+            
             Console.WriteLine("About to register client...");
-            await client.RegisterPlayer(name: "MohammadReza");
+            ConsoleLogging.Client_Info(await ClientInfo());
         }
 
-        
     }
 }
