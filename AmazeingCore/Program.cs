d4a328f92b2fdc16dc5be7468d8231289ac410ef
Mrbh <Mrbhmr@gmail.com>
2021-03-23 14:50:21 +0100

    Cover 84% of Traverse Class with maze traversing of 3 maze: Exit, Reverse, PacMan

--- AmazeingCore/Program.cs
+++ AmazeingCore/Program.cs
@@ -14,7 +14,7 @@ namespace AmazeingCore
         {
             try
             {
-                Traverse.Client = await Connection_Initialization();
+                Traverse.Client = await Connection_Initialization("MohammadReza");
                 await Traverse_Mazes();
                 Console.ReadLine();
             }
@@ -36,7 +36,7 @@ namespace AmazeingCore
                 }
                 catch (Exception e)
                 {
-                    ConsoleLogging.ExceptionHandler(e,$"Traversing Maze \"{maze.Name}\"");
+                    ConsoleLogging.ExceptionHandler(e, $"Traversing Maze \"{maze.Name}\"");
                 }
             }
 
@@ -44,18 +44,18 @@ namespace AmazeingCore
             ConsoleLogging.Client_Info(await ClientInfo());
         }
 
-        public static async Task<AmazeingClient> Connection_Initialization()
+        public static async Task<AmazeingClient> Connection_Initialization(string playerName, string token = null)
         {
             try
             {
                 var httpClient = new HttpClient();
-                var authorization_Key = "HTI Thanks You [e48a]";
+                var authorization_Key = token ?? "HTI Thanks You [e48a]";
 
                 httpClient.DefaultRequestHeaders.Add("Authorization", authorization_Key);
                 _client = new AmazeingClient("https://maze.hightechict.nl/", httpClient);
 
                 await _client.ForgetPlayer();
-                await _client.RegisterPlayer(name: "MohammadReza");
+                await _client.RegisterPlayer(name: playerName);
 
                 Console.WriteLine("About to register client...");
                 ConsoleLogging.Client_Info(await ClientInfo());
--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -1,22 +1,45 @@
+using System.Linq;
 using System.Threading.Tasks;
+using AmazeingCore;
 using Xunit;
 
 namespace UnitTests
 {
     public class UnitTests
     {
+
+
         [Theory]
         [InlineData("MohammadReza")]
         public async Task Program_ConnectionInitialization_CorrectPlayerNameFromAPI(string playerName)
         {
             //Arrange & Act 
-            var _client = await AmazeingCore.Program.Connection_Initialization();
+            var _client = await AmazeingCore.Program.Connection_Initialization(playerName);
             var clientInfo = _client.GetPlayerInfo();
 
             ////Assert
             Assert.Equal(playerName, clientInfo.Result.Name);
         }
 
-        
+        [Theory]
+        [InlineData("Exit")]
+        [InlineData("Reverse")]
+        [InlineData("PacMan")]
+        public async Task Traverse_OnSpecificMaze_AllPointCollected(string mazeName)
+        {
+            //Arrange
+            Traverse.Client = await AmazeingCore.Program.Connection_Initialization("Someone");
+            var mazeList = await Traverse.Client.AllMazes();
+            var maze = mazeList.Single(m => m .Name == mazeName);
+            
+            //Act
+            await Traverse.Start(maze);
+            var playerScore = (await Traverse.Client.GetPlayerInfo()).PlayerScore;
+
+            //Assert
+            Assert.Equal(maze.PotentialReward, playerScore);
+        }
+
+
     }
 }
